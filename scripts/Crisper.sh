5ea86d15350576ddebdc7cbed28c9d86e68ce10c
yoshiaki <yoshiaki@yoshiakinoMac-Studio.local>
2025-05-01 13:44:12 +0900

    Add GTDB-Tk classification script

--- scripts/Crisper.sh
+++ scripts/Crisper.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Input and output
+input_file="your_genome.fa"
+output_dir="crispr_output"
+
+# Extract genome name from input filename (remove .fa extension)
+genome_name=$(basename "$input_file" .fa)
+
+# Run CRISPRCasFinder using Singularity
 singularity exec CrisprCasFinder.simg perl /usr/local/CRISPRCasFinder/CRISPRCasFinder.pl \
   -so /usr/local/CRISPRCasFinder/sel392v2.so \
   -cf /usr/local/CRISPRCasFinder/CasFinder-2.0.3 \
@@ -7,7 +17,8 @@ singularity exec CrisprCasFinder.simg perl /usr/local/CRISPRCasFinder/CRISPRCasF
   -out "$output_dir" \
   -in "$input_file"
 
+# Extract spacers with Evidence Level 3 or 4
 cat "$output_dir/result.json" | jq -r \
   '.Sequences[].Crisprs[] | select(.Evidence_Level == 4 or .Evidence_Level == 3) | .Regions[] | select(.Type == "Spacer") | .Sequence' \
-  | awk -v dir_name="$dir_name" '{print ">" dir_name "_Sequence" NR "\n" $0}' \
+  | awk -v genome_name="$genome_name" '{print ">" genome_name "_Sequence" NR "\n" $0}' \
   > "$output_dir/spacer.fasta"
